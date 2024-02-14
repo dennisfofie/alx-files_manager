9d0a1667cbe80e43d98e561b97a009a159b8c55c
Dennis Fofie <dfofie@tapnhop.com>
2024-02-14 10:54:17 +0000

    final commit

---
+++ controllers/AppController.js
@@ -0,0 +1,23 @@
+const redisClient = require('../utils/redis');
+const dbClient = require('../utils/db');
+
+class AppController {
+  static getStatus(request, response) {
+    const status = {
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
+    };
+    response.status(200).send(status);
+  }
+
+  static async getStats(request, response) {
+    const stats = {
+      users: await dbClient.nbUsers(),
+      files: await dbClient.nbFiles(),
+    };
+    response.status(200).send(stats);
+  }
+}
+
+module.exports = AppController;
+
